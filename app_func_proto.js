e253a29086e1df2cd80deca111d785b54cec559d
Kaiser Alam <Kaiser@DESKTOP-J17JR13>
2019-01-02 11:52:49 -0800

    added module export

---
+++ app_func_proto.js
@@ -0,0 +1,4 @@
+let person = require('./func_proto');
+
+let alam = new person('Kaiser', 'Alam');
+console.log(alam.firstname + ' ' + alam.lastname + '_' + alam.currenttime());
--- func_proto.js
+++ func_proto.js
@@ -1,15 +1,15 @@
-function Person(firstname, lastname, eyecolor){
+let person = function(firstname, lastname){
+
   this.firstname = firstname;
   this.lastname = lastname;
-  this.eyecolor = eyecolor;
+
+  this.fullname = function(){
+    return this.firstname + ' ' + this.lastname;
+  }
+
+  this.currenttime = function(){
+    return new Date().getTime();
+  }
 }
 
-Person.prototype.nationality = "Bangladesh";
-Person.prototype.getFullName = function(){
-  return this.firstname + ' ' + this.lastname;
-}
-
-let Alam = new Person("Kaiser", "Alam", "Black");
-
-console.log(Alam.nationality);
-console.log(Alam.getFullName());
+module.exports = person;
