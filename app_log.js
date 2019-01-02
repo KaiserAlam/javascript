1698cf786b76ff173955391e08deb444a90d18f2
Kaiser Alam <Kaiser@DESKTOP-J17JR13>
2019-01-02 12:01:42 -0800

    added logging module

---
+++ app_log.js
@@ -0,0 +1,3 @@
+let app_log = require('./log');
+
+app_log.info('This is an info message: ');
---
+++ log.js
@@ -0,0 +1,15 @@
+let log = {
+
+  info: function(info){
+    console.log('Info: ' + info);
+  },
+
+  warning: function(warn){
+    console.log('Warning: ' + warn);
+  },
+  error: function(err){
+    console.log('Error: ' + err);
+  }
+};
+
+module.exports = log;
